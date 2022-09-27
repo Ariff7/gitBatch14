e54db91a04052585a2893655624bb5a96f61cccc
Ariff7 <ariffaliyev@gmail.com>
2022-09-27 18:50:51 -0500

    Second commit

--- src/Class03B14/Class03.java
+++ src/Class03B14/Class03.java
@@ -3,7 +3,10 @@ package Class03B14;
 public class Class03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+System.out.println("Day 3 of git ! Let's get it! ");
+
+System.out.println("This is a change in my project");
 
 	}
 
